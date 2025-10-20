523b279ed3efae5bab62a984cdfe5dd459cc82ce
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2025-10-20 16:45:50 +0330

    feat: create models

--- credit/salesman/admin.py
+++ credit/salesman/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Salesman, CreditRequest, PhoneNumber, CreditTransfer
+
+
+admin.site.register(Salesman)
+admin.site.register(CreditRequest)
+admin.site.register(PhoneNumber)
+admin.site.register(CreditTransfer)
--- credit/salesman/models.py
+++ credit/salesman/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Salesman(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class CreditRequest(models.Model):
+    salesman = models.ForeignKey(to=Salesman, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    state = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.amount} - {self.salesman} ({self.state})'
+
+
+class PhoneNumber(models.Model):
+    number = models.CharField(max_length=11)
+
+    def __str__(self):
+        return self.number
+
+
+class CreditTransfer(models.Model):
+    salesman = models.ForeignKey(to=Salesman, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    phone_number = models.ForeignKey(to=PhoneNumber, on_delete=models.CASCADE)  # CASCADE?? what if I use SET_NULL??
+
+    def __str__(self):
+        return f'{self.amount} - {self.salesman} ({self.phone_number})'
