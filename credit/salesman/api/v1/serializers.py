80ed8aa9415f70423e15d05abdb30aaa6fde6436
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2025-10-22 15:16:27 +0330

    chore: validate credit transfer amount

--- credit/salesman/api/v1/serializers.py
+++ credit/salesman/api/v1/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from salesman.models import CreditRequest, CreditTransfer
+from salesman.models import CreditRequest, CreditTransfer, Salesman
 
 
 class CreditRequestSerializer(serializers.ModelSerializer):
@@ -13,3 +13,10 @@ class CreditTransferSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = CreditTransfer
+
+    def is_valid(self, *, raise_exception=False):
+        result = super(CreditTransferSerializer, self).is_valid(raise_exception=raise_exception)
+        salesman = Salesman.objects.annotate_total_credit().get(id=self.initial_data['salesman'])
+        if salesman.total_credit < self.initial_data['amount']:
+            raise serializers.ValidationError({'amount': 'amount is greater than your credit'})
+        return result
