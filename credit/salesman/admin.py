ea39c3e93c51c9d47d09ea50bc24562ee4e07029
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2025-10-25 02:41:05 +0330

    chore: update admin panel

--- credit/salesman/admin.py
+++ credit/salesman/admin.py
@@ -3,7 +3,29 @@ from django.contrib import admin
 from .models import Salesman, CreditRequest, PhoneNumber, CreditTransfer
 
 
-admin.site.register(Salesman)
-admin.site.register(CreditRequest)
+class CreditRequestInline(admin.TabularInline):
+    model = CreditRequest
+
+
+class CreditTransferInline(admin.TabularInline):
+    model = CreditTransfer
+
+
+class SalesmanAdmin(admin.ModelAdmin):
+    inlines = [CreditRequestInline, CreditTransferInline]
+
+
+class CreditRequestAdmin(admin.ModelAdmin):
+    list_filter = ['salesman', 'state']
+    search_fields = ['salesman__name', 'amount']
+
+
+class CreditTransferAdmin(admin.ModelAdmin):
+    list_filter = ['salesman']
+    search_fields = ['salesman__name', 'amount', 'phone_number__number']
+
+
+admin.site.register(Salesman, SalesmanAdmin)
+admin.site.register(CreditRequest, CreditRequestAdmin)
 admin.site.register(PhoneNumber)
-admin.site.register(CreditTransfer)
+admin.site.register(CreditTransfer, CreditTransferAdmin)
