1d954ddb1608fcf0d9f84922a7bf793aa32094a6
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2025-10-25 02:24:07 +0330

    chore: use postgres for database

--- credit/credit/settings.py
+++ credit/credit/settings.py
@@ -75,11 +75,22 @@ WSGI_APPLICATION = 'credit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'credit',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PASSWORD': '1234',
+        'PORT': '5432',
+    },
 }
 
 
