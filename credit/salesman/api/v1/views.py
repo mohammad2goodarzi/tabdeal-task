415bf66644d7c42b96189f20115575dfd9336ced
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2025-10-25 13:23:55 +0330

    chore: handle race condition

--- credit/salesman/api/v1/views.py
+++ credit/salesman/api/v1/views.py
@@ -4,7 +4,13 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from salesman.api.v1.serializers import CreditRequestSerializer, CreditTransferSerializer
+from salesman.controllers import CreditController
 from salesman.models import CreditRequest, CreditTransfer
+
+
+class TempAPIView(APIView):
+    def get(self, *args, **kwargs):
+        return Response('hello world')
 
 
 class CreditRequestAPIView(CreateAPIView):
@@ -17,10 +23,17 @@ class CreditAcceptAPIView(GenericAPIView):
 
     def post(self, request, *args, **kwargs):
         instance = self.get_object()
-        instance.set_as_accepted()
+        CreditController().accept(instance)
         data = self.get_serializer(instance=instance).data
         return Response(data=data, status=status.HTTP_200_OK)
 
 
 class CreditTransferAPIView(CreateAPIView):
     serializer_class = CreditTransferSerializer
+
+    def perform_create(self, serializer):
+        CreditController().transfer(
+            salesman_id=serializer.data['salesman'],
+            amount=serializer.data['amount'],
+            phone_number_id=serializer.data['phone_number'],
+        )
---
+++ credit/salesman/controllers.py
@@ -0,0 +1,23 @@
+from django.db import transaction
+
+from salesman.models import CreditTransfer, Salesman
+
+
+class CreditController:
+    def accept(self, db_obj):
+        with transaction.atomic():
+            db_obj.set_as_accepted()
+            salesman = Salesman.objects.select_for_update().get(pk=db_obj.salesman.pk)
+            salesman.total_credit += db_obj.amount
+            salesman.save()
+
+    def transfer(self, salesman_id, amount, phone_number_id):
+        with transaction.atomic():
+            ct = CreditTransfer.objects.create(
+                salesman_id=salesman_id,
+                amount=amount,
+                phone_number_id=phone_number_id,
+            )
+            salesman = Salesman.objects.select_for_update().get(pk=ct.salesman.pk)
+            salesman.total_credit -= ct.amount
+            salesman.save()
