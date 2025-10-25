87e20207dd02cd802122c018d7adcb3bb36a2e9f
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2025-10-25 13:24:18 +0330

    chore: validate salesman correctly

--- credit/salesman/api/v1/serializers.py
+++ credit/salesman/api/v1/serializers.py
@@ -16,7 +16,7 @@ class CreditTransferSerializer(serializers.ModelSerializer):
 
     def is_valid(self, *, raise_exception=False):
         result = super(CreditTransferSerializer, self).is_valid(raise_exception=raise_exception)
-        salesman = Salesman.objects.annotate_total_credit().get(id=self.initial_data['salesman'])
+        salesman = Salesman.objects.get(id=self.initial_data['salesman'])
         if salesman.total_credit < self.validated_data['amount']:
             raise serializers.ValidationError({'amount': 'amount is greater than your credit'})
         return result
